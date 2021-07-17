098e3d6f8b3937bd9f0735548d0e2a7c904ecb5d
Iluvcapsizin <48321540+Iluvcapsizin@users.noreply.github.com>
2021-07-17 13:50:41 +0800

    Update RTD.py

--- RTD.py
+++ RTD.py
@@ -20,6 +20,8 @@ import matplotlib.pyplot as plt
 import plotly as plot
 #Instasll rtdpy first! (TRY INSTALL MANUALLY INSTEAD OF PIP) idk why it installed older version
 import rtdpy
+import time
+
 
 class pulse:
     def __init__(self,V_reactor,flow):
@@ -46,20 +48,84 @@ class pulse:
     '''
 
     #rtdpy not as nice as signal.unit_impulse for PFR
-    #pfr = rtdpy.Pfr(tau = tau, dt=.01, time_end=100)
-    #plt.plot(pfr.time, pfr.exitage)
-    def PFR(self):
-        imp = signal.unit_impulse(100, int(self.tau))
-        plt.plot(np.arange(0, 100), imp)
+    # 3 methods for ideal PFR RTD:
+    #   1) pfr = rtdpy.Pfr(tau = tau, dt=.01, time_end=100)
+    #      plt.plot(pfr.time, pfr.exitage)
+    #   2) imp = signal.unit_impulse(100)
+    #      plt.plot(np.arange(0, 100), imp)
 
+    def PFR(self):
+
+        xdata = []
+        ydata = []
+
+        plt.show()
+        
+        axes = plt.gca()
+        axes.set_xlim(0, 100)
+        axes.set_ylim(0,1.4)
+        line, = axes.plot(xdata, ydata, 'r-')
         plt.margins(0.1,0.1)
         plt.xlabel('Time')
         plt.ylabel('E',rotation=0)
         plt.grid(True)
+        plt.title('PFR - Pulse: E against T')
+
+        for i in range(100):
+            if i != self.tau:
+                xdata.append(i)
+                ydata.append(0)
+            else:
+                xdata.append(i)
+                ydata.append(1)
+            line.set_xdata(xdata)
+            line.set_ydata(ydata)
+            plt.draw()
+            plt.pause(1e-20)
+            time.sleep(0.1)
+
         plt.show()
 
     def CSTR(self,n):
         #n = number of tanks, so we can allow them to select number of CSTRs
+        xdata = []
+        ydata = []
+        #plt.show()
+
+        axes = plt.gca()
+        axes.set_xlim(0, 100)
+        axes.set_ylim(0,0.05)
+        line, = axes.plot(xdata, ydata, 'r-')
+        plt.margins(0.1,0.1)
+        plt.xlabel('Time')
+        plt.ylabel('E',rotation=0)
+        plt.grid(True)
+        plt.title('nCSTR - Pulse: E against T')
+        plt.legend()
+
+        if type(n) == int:
+            CSTR = rtdpy.Ncstr(tau=self.tau, n=n, dt=.01, time_end=100)
+            for i in range(len(CSTR.time)):
+                xdata.append(CSTR.time[i])
+                ydata.append(CSTR.exitage[i])
+                print(xdata)
+                print(ydata)
+                line.set_xdata(xdata)
+                line.set_ydata(ydata)
+                plt.draw()
+                plt.pause(1e-20)
+                time.sleep(0.1)
+            #plt.show()
+        if type(n) == list or type(n) == tuple:
+            for n in n:
+                CSTR = rtdpy.Ncstr(tau=self.tau, n=n, dt=.01, time_end=100)
+                plt.plot(CSTR.time, CSTR.exitage, label=f"n={n}")
+        else:
+            pass
+
+
+    def CSTR1(self,n):
+       #n = number of tanks, so we can allow them to select number of CSTRs
         if type(n) == int:
             CSTR = rtdpy.Ncstr(tau=self.tau, n=n, dt=.01, time_end=100)
             plt.plot(CSTR.time, CSTR.exitage, label=f"n={n}")
@@ -68,15 +134,14 @@ class pulse:
                 CSTR = rtdpy.Ncstr(tau=self.tau, n=n, dt=.01, time_end=100)
                 plt.plot(CSTR.time, CSTR.exitage, label=f"n={n}")
         else:
-            print("lmao list or int plz")
+            pass
 
         plt.xlabel('Time')
-        plt.ylabel('E')
+        plt.ylabel('E',rotation=0)
         plt.legend()
         plt.show()
 
-a = pulse(50,1)
-t = np.array([0, 1, 2, 3, 4, 5])
-C = np.array([0.5, 0.2, 0.3, 0.15, 0.1, 0.0])
-a.CSTR((2,))
+    
 
+a = pulse(50,1)
+a.CSTR1(1)
